35289270100017213f8e25b0ffd44f5e64d15caa
fh8 <67806961+GreyWolf4389@users.noreply.github.com>
2023-09-26 11:34:21 -0400

    Update Sep26.R

--- Gao/Unit 3/Sep26.R
+++ Gao/Unit 3/Sep26.R
@@ -4,3 +4,21 @@ library(datasets)
 library(RJSONIO)
 
 diamonds
+
+#Rename columns to use title case
+
+names(diamonds) <- names(diamonds) %>% str_to_title()
+diamonds
+
+#check for NA values
+
+apply(X=diamonds,MARGIN=2,FUN=function(col),sum(is.na(col)))
+
+#Drop rows that have invalid values
+
+diamonds <- diamonds %>% filter(X!=0 & Y!=0 & Z!=0)
+diamonds
+
+#Make box plot
+
+ggplot(diamonds, aes(x=X,y=Price,fill=Cut)) + geom_boxplot()
