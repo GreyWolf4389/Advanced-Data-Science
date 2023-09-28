071900329e5050d2feb433207a11542e9fa45410
fh8 <67806961+GreyWolf4389@users.noreply.github.com>
2023-09-28 10:37:36 -0400

    Update Sep26.R

--- Gao/Unit 3/Sep26.R
+++ Gao/Unit 3/Sep26.R
@@ -26,3 +26,29 @@ ggplot(diamonds, aes(x=X,y=Price,fill=Cut)) + geom_boxplot()
 ggplot(diamonds, aes(x=X,y=Price,fill=Clarity)) + geom_boxplot()
 
 ggplot(diamonds, aes(x=X,y=Price,fill=Color)) + geom_boxplot()
+
+#KDE plots with outlier fences
+
+get_upper_fence <- function(x) {
+  quantile(x, 0.75) + (1.5 * IQR(x))
+}
+
+get_lower_fence <- function(x) {
+  quantile(x, 0.25) - (1.5 * IQR(x))
+}
+
+diamonds_long <- diamonds %>%
+  select_if(is.numeric) %>%
+  pivot_longer(cols = names(select_if(diamonds, is.numeric)),
+               names_to = "Variable",
+               values_to = "Value") %>%
+  group_by(Variable) %>%
+  mutate(UpperFence = get_upper_fence(Value),
+         LowerFence = get_lower_fence(Value))
+
+ggplot(data = diamonds_long, aes(x = Value)) + 
+  geom_density(aes(color = Variable)) +
+  geom_vline(aes(xintercept = UpperFence), color = "red") + 
+  geom_vline(aes(xintercept = LowerFence), color = "red") +
+  facet_wrap(facets =  vars(Variable), scale = "free") +
+  guides(color = "none")
